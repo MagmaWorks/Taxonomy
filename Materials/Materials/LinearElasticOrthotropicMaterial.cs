502408c5977dcf385d5cc7e2a05e49a5d57576d2
kpne <kpn@kpne.dk>
2024-09-14 21:23:47 +0200

    materials implementation

---
+++ Materials/Materials/LinearElasticOrthotropicMaterial.cs
@@ -0,0 +1,28 @@
+﻿using MagmaWorks.Taxonomy.Materials;
+using OasysUnits;
+
+namespace Materials
+{
+    public class LinearElasticOrthotropicMaterial : ILinearElasticOrthotropicMaterial
+    {
+        public Pressure ElasticModulusX { get; set; }
+        public Pressure ElasticModulusY { get; set; }
+        public Pressure ElasticModulusZ { get; set; }
+        public Pressure StrengthX { get; set; }
+        public Pressure StrengthY { get; set; }
+        public Pressure StrengthZ { get; set; }
+        public MaterialType Type { get; set; }
+
+        public LinearElasticOrthotropicMaterial(MaterialType type, Pressure elasticModulusX, Pressure strengthX,
+            Pressure elasticModulusY, Pressure strengthY, Pressure elasticModulusZ, Pressure strengthZ)
+        {
+            Type = type;
+            ElasticModulusX = elasticModulusX;
+            StrengthX = strengthX;
+            ElasticModulusY = elasticModulusY;
+            StrengthY = strengthY;
+            ElasticModulusZ = elasticModulusZ;
+            StrengthZ = strengthZ;
+        }
+    }
+}
---
+++ Materials/Materials/StandardMaterial.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using MagmaWorks.Taxonomy.Materials;
+using MagmaWorks.Taxonomy.Standards;
+
+namespace Materials
+{
+    public class StandardMaterial : IStandardMaterial
+    {
+        public IStandard Standard { get; set; }
+        public Enum Grade { get; set; }
+        public MaterialType Type { get; set; }
+
+        public StandardMaterial(MaterialType type, IStandard standard, Enum grade)
+        {
+            Type = type;
+            Standard = standard;
+            Grade = grade;
+        }
+    }
+}
