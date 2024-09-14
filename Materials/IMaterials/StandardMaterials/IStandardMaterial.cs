10f37fbefae7109176c9f8d8bfa08ec08661f7ab
kpne <kpn@kpne.dk>
2024-09-14 21:22:55 +0200

    Rename code to standard

---
+++ Materials/IMaterials/StandardMaterials/IStandardMaterial.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using MagmaWorks.Taxonomy.Standards;
+
+namespace MagmaWorks.Taxonomy.Materials
+{
+    public interface IStandardMaterial : IMaterial
+    {
+        IStandard Standard { get ; }
+        Enum Grade { get; }
+    }
+}
