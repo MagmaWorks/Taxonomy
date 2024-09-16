b8912c2a1a988dbc9c60dfd38a1323a28af00f59
kpne <kpn@kpne.dk>
2024-09-16 18:36:08 +0200

    parabola rectangle material

---
+++ Materials/IMaterials/AnalysisMaterials/IParabolaRectangleMaterial.cs
@@ -0,0 +1,14 @@
+﻿using OasysUnits;
+
+namespace MagmaWorks.Taxonomy.Materials
+{
+    public interface IParabolaRectangleMaterial : IAnalysisMaterial
+    {
+        Pressure YieldStrength { get; }
+        Strain YieldStrain { get; }
+        Strain FailureStrain { get; }
+        double Exponent { get; set; }
+
+        public Pressure StressAt(Strain strain);
+    }
+}
---
+++ Materials/Materials/AnalysisMaterials/ParabolaRectangleMaterial.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using OasysUnits;
+
+namespace MagmaWorks.Taxonomy.Materials
+{
+    public class ParabolaRectangleMaterial : IParabolaRectangleMaterial
+    {
+        public MaterialType Type { get; set; }
+        public Pressure YieldStrength { get; set; }
+        public Strain YieldStrain { get; set; }
+        public Strain FailureStrain { get; set; }
+        public double Exponent { get; set; }
+
+        public ParabolaRectangleMaterial(MaterialType type, Pressure yieldStrength, Strain yieldStrain, Strain failureStrain, double exponent)
+        {
+            Type = type;
+            YieldStrength = yieldStrength;
+            YieldStrain = yieldStrain;
+            FailureStrain = failureStrain;
+            Exponent = exponent;
+        }
+
+        public Pressure StressAt(Strain strain)
+        {
+            if (strain > FailureStrain)
+            {
+                throw new ArgumentException($"Input {strain} is larger than Failure Strain ({FailureStrain})");
+            }
+
+            if (strain >= YieldStrain)
+            {
+                return YieldStrength;
+            }
+
+            return YieldStrength * (1 - Math.Pow(1 - strain / YieldStrain, Exponent));
+        }
+    }
+}
