f03b8f27111a0565a2278ab95aa0be8bdcdcda0b
kpne <kpn@kpne.dk>
2024-09-17 11:25:37 +0200

    EN 1992 Rebar material

---
+++ Materials/IMaterials/StandardMaterials/EN/ENRebarGrade.cs
@@ -0,0 +1,12 @@
+﻿namespace MagmaWorks.Taxonomy.Materials
+{
+    public enum ENRebarGrade
+    {
+        B450A,
+        B450C,
+        B500A,
+        B500B,
+        B500C,
+        B550B
+    }
+}
