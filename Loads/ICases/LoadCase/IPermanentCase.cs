ff969d64d712da15dd0ff2e93d7445249f7cc833
kpne <kpn@kpne.dk>
2024-09-25 12:12:17 +0200

    summary and renaming

--- Loads/ICases/LoadCase/IPermanentCase.cs
+++ Loads/ICases/LoadCase/IPermanentCase.cs
@@ -4,7 +4,14 @@ namespace MagmaWorks.Taxonomy.Loads
 {
     public interface IPermanentCase : ILoadCase
     {
+        /// <summary>
+        /// γ_G,j,sup - Unfavourable (superior) partial factor
+        /// </summary>
         Ratio Unfavourable { get; }
+
+        /// <summary>
+        /// γ_G,j,inf - Favourable (inferior) partial factor
+        /// </summary>
         Ratio Favourable { get; }
     }
 }
--- Loads/ICases/LoadCase/IVariableCase.cs
+++ Loads/ICases/LoadCase/IVariableCase.cs
@@ -4,8 +4,19 @@ namespace MagmaWorks.Taxonomy.Loads
 {
     public interface IVariableCase : ILoadCase
     {
-        Ratio Characteristic { get; } // ψ_0
-        Ratio Frequent { get; } // ψ_1
-        Ratio QuasiPermanent { get; } // ψ_2
+        /// <summary>
+        /// ψ_0 Characteristic partial factor
+        /// </summary>
+        Ratio Characteristic { get; }
+
+        /// <summary>
+        /// ψ_1 Frequent partial factor
+        /// </summary>
+        Ratio Frequent { get; }
+
+        /// <summary>
+        /// ψ_2 Quasi-permanent partial factor
+        /// </summary>
+        Ratio QuasiPermanent { get; }
     }
 }
