46f1e6f62df45765c56bc37c97a64dd3459cc6db
Lint Action <lint-action@samuelmeuli.com>
2024-09-26 17:26:01 +0000

    Fix code style issues with dotnet_format

--- Loads/ICases/Combinations/IMemberDesignCombination.cs
+++ Loads/ICases/Combinations/IMemberDesignCombination.cs
@@ -2,7 +2,8 @@
 
 namespace MagmaWorks.Taxonomy.Loads
 {
-    public interface IMemberDesignCombination : IUltimateLimitState {
+    public interface IMemberDesignCombination : IUltimateLimitState
+    {
         Ratio PermanentPartialFactor { get; }
         Ratio LeadingVariablePartialFactor { get; }
         Ratio OtherVariablePartialFactor { get; }
