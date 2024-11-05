1ded648802fe1ada6d3ba9f90de1e33a026419ad
kpne <kpn@kpne.dk>
2024-11-05 13:16:35 +0100

    one liner

--- Serialization/Serialization/JsonSerializationExtensions.cs
+++ Serialization/Serialization/JsonSerializationExtensions.cs
@@ -6,21 +6,13 @@ namespace MagmaWorks.Taxonomy.Serialization
     {
         public static string ToJson<T>(this T taxonomyObject, JsonSerializerSettings? settings = null) where T : ITaxonomySerializable
         {
-            if (settings == null)
-            {
-                settings = TaxonomyJsonSerializer.Settings;
-            }
-
+            settings ??= TaxonomyJsonSerializer.Settings;
             return JsonConvert.SerializeObject(taxonomyObject, Formatting.Indented, settings);
         }
 
         public static T? FromJson<T>(this string json, JsonSerializerSettings? settings = null) where T : ITaxonomySerializable
         {
-            if (settings == null)
-            {
-                settings = TaxonomyJsonSerializer.Settings;
-            }
-
+            settings ??= TaxonomyJsonSerializer.Settings;
             return JsonConvert.DeserializeObject<T>(json, settings);
         }
     }
