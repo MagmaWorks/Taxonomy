b68e11ad4abf50514b4aeac8a1fae3afd817b91f
kpne <kpn@kpne.dk>
2024-11-15 17:27:42 +0100

    add custom exception

--- Sections/Sections/ConcreteSection.cs
+++ Sections/Sections/ConcreteSection.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using MagmaWorks.Taxonomy.Materials;
 using MagmaWorks.Taxonomy.Profiles;
+using MagmaWorks.Taxonomy.Sections.Exceptions;
 using OasysUnits;
 
 namespace MagmaWorks.Taxonomy.Sections
@@ -11,14 +12,45 @@ namespace MagmaWorks.Taxonomy.Sections
         public IRebar Link { get; set; }
         public Length Cover { get; set; } = Length.Zero;
 
-        public ConcreteSection(IProfile profile, IMaterial material) : base (profile, material) { }
+        public ConcreteSection(IProfile profile, IMaterial material) : base(profile, material)
+        {
+            if (material.Type != MaterialType.Concrete)
+            {
+                throw new InvalidMaterialTypeException("Material type must be Concrete");
+            }
+        }
 
         public ConcreteSection(IProfile profile, IMaterial material, IList<ILongitudinalReinforcement> rebars, IRebar link, Length cover)
             : base(profile, material)
         {
+            if (material.Type != MaterialType.Concrete)
+            {
+                throw new System.ArgumentException("Material type must be Concrete");
+            }
+
             Rebars = rebars;
             Link = link;
             Cover = cover;
+        }
+
+        public void AddRebars(IRebar rebar, int numberOfBars, SectionFace face)
+        {
+
+        }
+
+        public void AddRebars(IRebar rebar, Length spacing, SectionFace face)
+        {
+
+        }
+
+        public void AddRebars(IRebar rebar, int numberOfBars)
+        {
+
+        }
+
+        public void AddRebars(IRebar rebar, Length spacing)
+        {
+
         }
     }
 }
