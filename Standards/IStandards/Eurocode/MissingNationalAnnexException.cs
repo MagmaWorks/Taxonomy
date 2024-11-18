5a35b1cd764bb48013fbf90a02208223d036bcda
kpne <kpn@kpne.dk>
2024-11-18 16:52:38 +0100

    MissingNationalAnnexException

---
+++ Standards/IStandards/Eurocode/MissingNationalAnnexException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace MagmaWorks.Taxonomy.Standards.Eurocode
+{
+    public class MissingNationalAnnexException : Exception
+    {
+        public MissingNationalAnnexException(NationalAnnex na) : base($"National Annex of {na} not implemented") { }
+    }
+}
