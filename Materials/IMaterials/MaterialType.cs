5edd6cf9f269ec44ddbf2b4365fac997d72693f6
Til Reinhardt <t.reinhardt@whitbywood.com>
2024-12-11 15:08:23 +0100

    Solid and glued laminated timber

--- Materials/IMaterials/MaterialType.cs
+++ Materials/IMaterials/MaterialType.cs
@@ -2,15 +2,17 @@
 {
     public enum MaterialType
     {
-        Generic,
-        Concrete,
-        Steel,
-        Timber,
-        Masonry,
-        Aluminium,
-        Reinforcement,
-        FRP,
-        Glass,
-        Fabric
+        Generic = 0,
+        Concrete = 1,
+        Steel = 2,
+        Timber = 3,
+        Masonry = 4,
+        Aluminium = 5,
+        Reinforcement = 6,
+        FRP = 7,
+        Glass = 8,
+        Fabric = 9,
+        SolidTimber = 10,
+        GluedLaminatedTimber = 11
     }
 }
