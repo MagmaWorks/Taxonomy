fa946071199880936dc9ab1458e793788db3604c
Lint Action <lint-action@samuelmeuli.com>
2025-04-16 17:09:12 +0000

    Fix code style issues with dotnet_format

--- Loads/LoadTests/GlobalUsings.cs
+++ Loads/LoadTests/GlobalUsings.cs
@@ -3,6 +3,6 @@ global using MagmaWorks.Taxonomy.Loads.Cases;
 global using MagmaWorks.Taxonomy.Standards.Eurocode;
 global using UnitsNet;
 global using UnitsNet.Units;
+global using Xunit;
 global using Moment = UnitsNet.Torque;
 global using MomentUnit = UnitsNet.Units.TorqueUnit;
-global using Xunit;
