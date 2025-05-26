ec4aa24d3ccb9930aabd8cc1f4b73921b5a28dfc
kpne <kpn@kpne.dk>
2025-05-26 16:34:55 +0200

    ITaxonomySerializable

--- Sections/ISections/Reinforcement/IRebar.cs
+++ Sections/ISections/Reinforcement/IRebar.cs
@@ -1,8 +1,9 @@
 ﻿using MagmaWorks.Taxonomy.Materials;
+using MagmaWorks.Taxonomy.Serialization;
 
 namespace MagmaWorks.Taxonomy.Sections.Reinforcement
 {
-    public interface IRebar
+    public interface IRebar : ITaxonomySerializable
     {
         Length Diameter { get; }
         IMaterial Material { get; }
