c55463cb504f8898d478a88cd9d71388f2920320
Til Reinhardt <t.reinhardt@whitbywood.com>
2025-05-30 11:30:58 +0200

    Nullable steel specification

--- Materials/Materials/StandardMaterials/En/EnSteelSpecification.cs
+++ Materials/Materials/StandardMaterials/En/EnSteelSpecification.cs
@@ -2,14 +2,14 @@
 {
     public class EnSteelSpecification : IEnSteelSpecification
     {
-        public EnExecutionClass ExecutionClassforStaticOrLowSeismicLoading { get; set; } = EnExecutionClass.EXC3;
-        public EnExecutionClass ExecutionClassforFatigueOrHighSeismicLoading { get; set; } = EnExecutionClass.EXC3;
-        public EnSteelCorrosionResistance CorrosionResistance { get; set; } = EnSteelCorrosionResistance.None;
-        public EnSteelDeliveryCondition DeliveryCondition { get; set; } = EnSteelDeliveryCondition.AR;
-        public EnSteelFormingTemperature FormingTemperature { get; set; } = EnSteelFormingTemperature.HotRolled;
-        public EnSteelImpactTemperatureProperty ImpactTemperatureProperty { get; set; } = EnSteelImpactTemperatureProperty.None;
-        public EnSteelQuality Quality { get; set; } = EnSteelQuality.None;
-        public bool HollowSection { get; set; } = false;
+        public EnExecutionClass? ExecutionClassforStaticOrLowSeismicLoading { get; set; } = EnExecutionClass.EXC3;
+        public EnExecutionClass? ExecutionClassforFatigueOrHighSeismicLoading { get; set; } = EnExecutionClass.EXC3;
+        public EnSteelCorrosionResistance? CorrosionResistance { get; set; } = EnSteelCorrosionResistance.None;
+        public EnSteelDeliveryCondition? DeliveryCondition { get; set; } = EnSteelDeliveryCondition.AR;
+        public EnSteelFormingTemperature? FormingTemperature { get; set; } = EnSteelFormingTemperature.HotRolled;
+        public EnSteelImpactTemperatureProperty? ImpactTemperatureProperty { get; set; } = EnSteelImpactTemperatureProperty.None;
+        public EnSteelQuality? Quality { get; set; } = EnSteelQuality.None;
+        public bool? HollowSection { get; set; } = false;
 
         internal EnSteelSpecification() { }
 
