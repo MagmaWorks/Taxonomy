51cc0a75e7685db725e1e631e520a53b1be85b0c
Til Reinhardt <t.reinhardt@whitbywood.com>
2025-05-30 13:32:28 +0200

    Steel quality class and typos

--- Materials/IMaterials/StandardMaterials/En/IEnSteelSpecification.cs
+++ Materials/IMaterials/StandardMaterials/En/IEnSteelSpecification.cs
@@ -4,13 +4,13 @@ namespace MagmaWorks.Taxonomy.Materials.StandardMaterials.En
 {
     public interface IEnSteelSpecification : ITaxonomySerializable
     {
-        EnExecutionClass? ExecutionClassforStaticOrLowSeismicLoading { get; set; }
-        EnExecutionClass? ExecutionClassforFatigueOrHighSeismicLoading { get; set; }
+        EnExecutionClass? ExecutionClassForStaticOrLowSeismicLoading { get; set; }
+        EnExecutionClass? ExecutionClassForFatigueOrHighSeismicLoading { get; set; }
         EnSteelCorrosionResistance CorrosionResistance { get; set; }
         EnSteelDeliveryCondition? DeliveryCondition { get; set; }
         EnSteelFormingTemperature? FormingTemperature { get; set; }
         EnSteelImpactTemperatureProperty ImpactTemperatureProperty { get; set; }
-        EnSteelQuality Quality { get; set; }
+        EnSteelQualityClass QualityClass { get; set; }
         bool? HollowSection { get; set; }
     }
 }
