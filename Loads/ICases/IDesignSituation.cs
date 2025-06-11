6bcfb001eeb23f61b028af91037083fa9176c184
Til Reinhardt <t.reinhardt@whitbywood.com>
2025-06-11 16:46:02 +0200

    Comments

--- Loads/ICases/IDesignSituation.cs
+++ Loads/ICases/IDesignSituation.cs
@@ -3,12 +3,29 @@
     public interface IDesignSituation
     {
         DesignSituationClass Class { get; }
+        /// <summary>
+        /// γ_G,j,sup permanent actions unfavourable partial factor
+        /// </summary>
         double UnfavourablePermanentActionsPartialFactor { get; }
+        /// <summary>
+        /// γ_G,j,inf permanent actions favourable partial factor
+        /// </summary>
         double FavourablePermanentActionsPartialFactor { get; }
+        /// <summary>
+        /// γ_Q,1 leading variable/accidental/seismic action partial factor
+        /// </summary>
         double LeadingActionPartialFactor { get; }
+        /// <summary>
+        /// γ_Q,1 main accompanying variable actions partial factor
+        /// </summary>
         double MainAccompanyingVariableActionsPartialFactor { get; }
+        /// <summary>
+        /// γ_Q,i other accompanying variable actions partial factor
+        /// </summary>
         double OtherAccompanyingVariableActionsPartialFactor { get; }
+        /// <summary>
+        /// ξ permanent load reduction factor
+        /// </summary>
         double ReductionFactor { get; }
-        double PrestressPartialFactor { get; }
     }
 }
